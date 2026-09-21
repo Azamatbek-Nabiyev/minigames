f77e58c776cdb66cd9aedade9538cd1b15b56ac5
AZAMAT <azamatnabiyev77@gmail.com>
2026-09-21 14:02:17 +0500

    feat: add real game images to hero and carousel sections

--- src/shared/ui/carousel/carousel.data.ts
+++ src/shared/ui/carousel/carousel.data.ts
@@ -13,9 +13,33 @@ export interface GameCard {
 }
 
 export const carouselGames: GameCard[] = [
-  { id: 'islanders', title: 'ISLANDERS: New Shores', likes: '54.2K', rating: 4.9, image: islandersImage },
-  { id: 'vacation-cafe', title: 'Vacation Cafe Simulator', likes: '28.7K', rating: 4.8, image: vacationCafeImage },
-  { id: 'winter-burrow', title: 'Winter Burrow', likes: '32.4K', rating: 4.9, image: winterBurrowImage },
-  { id: 'shelve-potions', title: 'Shelve the Potions!', likes: '21.3K', rating: 4.7, image: shelvePotionsImage },
+  {
+    id: 'islanders',
+    title: 'ISLANDERS: New Shores',
+    likes: '54.2K',
+    rating: 4.9,
+    image: islandersImage,
+  },
+  {
+    id: 'vacation-cafe',
+    title: 'Vacation Cafe Simulator',
+    likes: '28.7K',
+    rating: 4.8,
+    image: vacationCafeImage,
+  },
+  {
+    id: 'winter-burrow',
+    title: 'Winter Burrow',
+    likes: '32.4K',
+    rating: 4.9,
+    image: winterBurrowImage,
+  },
+  {
+    id: 'shelve-potions',
+    title: 'Shelve the Potions!',
+    likes: '21.3K',
+    rating: 4.7,
+    image: shelvePotionsImage,
+  },
   { id: 'heartopia', title: 'Heartopia', likes: '46.8K', rating: 4.6, image: heartopiaImage },
 ];
